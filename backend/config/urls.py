eeff6842ac5f64d53846aaef7ea224af8f621bb6
Vania Graciella Kwee <vaniakwee@gmail.com>
2025-11-30 17:11:51 +0800

    Initial push

---
+++ backend/config/urls.py
@@ -0,0 +1,13 @@
+
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic import RedirectView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("api/", include("TripMateFunctions.urls")),
+    
+
+
+
+]
---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [msg, setMsg] = useState("Loading...");
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/api/hello/")
+      .then((res) => res.json())
+      .then((data) => setMsg(data.message))
+      .catch(() => setMsg("Error calling API"));
+  }, []);
+
+  return (
+    <div>
+      <p>{msg}</p>
+    </div>
+  );
+}
+
+export default App;
